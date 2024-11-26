46e1001d9a32b081dc3ddcee6f23073ee4a57100
Saith_Ahmad <ahmadwebcrafts@gmail.com>
2024-11-27 02:03:09 +0500

    make space adjustable in right sidebar, by decrease gap

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -24,7 +24,7 @@ async function RightSidebar() {
           Suggested Communities
         </h3>
 
-        <div className='mt-7 flex w-[220px] flex-col gap-9'>
+        <div className='mt-7 flex w-[220px] flex-col gap-2'>
           {suggestedCOmmunities.communities.length > 0 ? (
             <>
               {suggestedCOmmunities.communities.map((community) => (
@@ -48,7 +48,7 @@ async function RightSidebar() {
 
       <div className='flex flex-1 flex-col justify-start'>
         <h3 className='text-heading4-medium text-light-1'>Similar Minds</h3>
-        <div className='mt-7 flex w-[220px] flex-col gap-10'>
+        <div className='mt-7 flex w-[220px] flex-col gap-2'>
           {similarMinds.users.length > 0 ? (
             <>
               {similarMinds.users.map((person) => (
